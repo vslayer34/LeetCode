2b66a860fbfc82f5d7710b38ecf2a065d00bcbfd
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-03-20 19:48:15 +0200

    Solved 2235.Add Two Integers

---
+++ 2235. Add Two Integers/2235. Add Two Integers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class ClassName
+{
+    public static void Main(string[] args)
+    {
+        var solution = new Solution();
+        
+        // Example 1
+        Helper.DisplayExample(
+            "num1 = 12, num2 = 5",
+            "17");
+        
+        Console.WriteLine(solution.Sum(12, 5));
+        Console.WriteLine();
+
+
+        // Example 2
+        Helper.DisplayExample(
+            "num1 = -10, num2 = 4",
+            "-6");
+       
+        Console.WriteLine(solution.Sum(-10, 4));
+        Console.WriteLine();
+    }
+}

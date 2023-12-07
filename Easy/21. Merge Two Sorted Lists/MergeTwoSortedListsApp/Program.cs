4217d15aa2334ad4d6ef4111c50a2219330dd8e1
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-07 18:02:40 +0200

    Update MergeTwoSortedListsProblem

---
+++ Easy/21. Merge Two Sorted Lists/MergeTwoSortedListsApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MergeTwoSortedListsApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        ListNode list1;
+        ListNode list2;
+        // Example 1
+        // Input: list1 = [1,2,4], list2 = [1,3,4]
+        // Output: [1,1,2,3,4,4]
+        {
+            list1 = new(1, new(2, new(4)));
+            list2 = new ListNode(1, new(3, new(4)));
+
+            MergeTwoLists(list1, list2);
+        }
+    }
+
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode newList = new();
+        
+        
+        throw new NotImplementedException();
+    }
+}
+
+
+
+class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
